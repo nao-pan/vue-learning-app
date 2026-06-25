0c575fb063bc276b33bf3a19f2a6e0214a60efe5
nao-pan <n.hasegawa.su@gmail.com>
2026-06-25 16:01:24 +0900

    ユーザー一覧（API連携）errorMessage,noResult追加実装

--- src/components/views/user/UserView.vue
+++ src/components/views/user/UserView.vue
@@ -3,8 +3,11 @@ import { useUsers } from '@/composables/useUsers';
 import { onMounted } from 'vue';
 
 const {
+  username,
   users,
   loading,
+  errorMessage,
+  noResult,
   fetchUsers,
 } = useUsers()
 
@@ -12,8 +15,20 @@ onMounted(() => {
   fetchUsers()
 })
 </script>
+
 <template>
   <h1>ユーザー一覧</h1>
+
+  <input v-model="username" placeholder="ユーザー名" />
+  <button @click="fetchUsers" @keyup.enter="fetchUsers">検索</button>
+
+  <p v-if="errorMessage" class="error">
+    {{ errorMessage }}
+  </p>
+
+  <p v-if="noResult">
+    該当するユーザーが見つかりません
+  </p>
 
   <div v-if="loading">
     Loading...
@@ -24,3 +39,9 @@ onMounted(() => {
     </li>
   </ul>
 </template>
+
+<style>
+.error {
+  color: red;
+}
+</style>
--- src/composables/useUsers.js
+++ src/composables/useUsers.js
@@ -1,20 +1,46 @@
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 export function useUsers() {
+  const username = ref('')
   const users = ref([])
   const loading = ref(false)
+  const errorMessage = ref('')
+
+  const noResult = computed(() => {
+    return !loading.value && users.value.length === 0
+  })
 
   const fetchUsers = async () => {
     loading.value = true
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    users.value = await response.json()
+    errorMessage.value = ''
 
-    loading.value = false
+    try {
+      const url = username.value.trim()
+        ? `https://jsonplaceholder.typicode.com/users?username=${username.value}`
+        : 'https://jsonplaceholder.typicode.com/users'
+
+      const response = await fetch(url)
+
+      if (!response.ok) {
+        throw new Error('取得失敗')
+      }
+
+      users.value = await response.json()
+    } catch (error) {
+      console.error('ユーザー取得失敗', error)
+
+      errorMessage.value = 'ユーザー取得に失敗しました'
+    } finally {
+      loading.value = false
+    }
   }
 
   return {
+    username,
     users,
     loading,
+    errorMessage,
+    noResult,
     fetchUsers,
   }
 }
